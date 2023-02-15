c1ca4393b5629442adc242abd0d5bcdf807af92c
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2023-02-15 20:30:13 +0100

    Turn WorldPlaceholder into a StringPlaceholder

--- common/src/main/java/com/ptsmods/chattix/placeholder/placeholders/WorldPlaceholder.java
+++ common/src/main/java/com/ptsmods/chattix/placeholder/placeholders/WorldPlaceholder.java
@@ -1,15 +1,15 @@
 package com.ptsmods.chattix.placeholder.placeholders;
 
-import com.ptsmods.chattix.placeholder.ComponentPlaceholder;
 import com.ptsmods.chattix.placeholder.PlaceholderContext;
+import com.ptsmods.chattix.placeholder.StringPlaceholder;
 import lombok.NonNull;
 import net.kyori.adventure.text.Component;
 import net.minecraft.server.level.ServerPlayer;
 import org.jetbrains.annotations.Nullable;
 
-public class WorldPlaceholder implements ComponentPlaceholder {
+public class WorldPlaceholder implements StringPlaceholder {
     @Override
-    public Component parse(@NonNull PlaceholderContext context, @NonNull ServerPlayer player, @NonNull Component message, @Nullable String arg) {
-        return Component.text(player.getLevel().dimension().location().toString());
+    public String parse(@NonNull PlaceholderContext context, @NonNull ServerPlayer player, @NonNull Component message, @Nullable String arg) {
+        return player.getLevel().dimension().location().toString();
     }
 }
