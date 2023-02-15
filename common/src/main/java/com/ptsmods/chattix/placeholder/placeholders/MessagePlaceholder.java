d1eee8caa8f8d2832bafb477ada1d80b2f7f7ca8
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2023-02-15 20:29:54 +0100

    Don't escape preprocess tag anymore as it no longer exists

--- common/src/main/java/com/ptsmods/chattix/placeholder/placeholders/MessagePlaceholder.java
+++ common/src/main/java/com/ptsmods/chattix/placeholder/placeholders/MessagePlaceholder.java
@@ -6,14 +6,10 @@ import lombok.NonNull;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.minimessage.MiniMessage;
-import net.kyori.adventure.text.minimessage.tag.Tag;
 import net.minecraft.server.level.ServerPlayer;
 import org.jetbrains.annotations.Nullable;
 
 public class MessagePlaceholder implements ComponentPlaceholder {
-    private static final MiniMessage miniMessage = MiniMessage.builder()
-            .editTags(b -> b.tag("preprocess", (arg, ctx) -> Tag.preProcessParsed(arg.popOr("No arg").value())))
-            .build();
 
     @Override
     public Component parse(@NonNull PlaceholderContext context, @NonNull ServerPlayer player, @NonNull Component message, @Nullable String arg) {
@@ -28,7 +24,7 @@ public class MessagePlaceholder implements ComponentPlaceholder {
     private Component escapeMMTags(Component component) {
         // Using our own MiniMessage instance so preprocess tags are also escaped.
         Component newComp = component instanceof TextComponent text ?
-                Component.text(miniMessage.escapeTags(text.content()))
+                Component.text(MiniMessage.miniMessage().escapeTags(text.content()))
                         .style(component.style()) : component;
 
         return newComp.children(component.children().stream()
