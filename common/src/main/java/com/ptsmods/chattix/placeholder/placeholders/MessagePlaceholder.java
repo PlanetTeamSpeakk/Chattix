c7e0ba10bc6574c81e5ce6c8846ab51dae334b1f
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2023-02-16 20:56:19 +0100

    Don't escape minimessage tags as they won't be parsed anyway

--- common/src/main/java/com/ptsmods/chattix/placeholder/placeholders/MessagePlaceholder.java
+++ common/src/main/java/com/ptsmods/chattix/placeholder/placeholders/MessagePlaceholder.java
@@ -4,8 +4,6 @@ import com.ptsmods.chattix.placeholder.ComponentPlaceholder;
 import com.ptsmods.chattix.placeholder.PlaceholderContext;
 import lombok.NonNull;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.TextComponent;
-import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.minecraft.server.level.ServerPlayer;
 import org.jetbrains.annotations.Nullable;
 
@@ -13,21 +11,6 @@ public class MessagePlaceholder implements ComponentPlaceholder {
 
     @Override
     public Component parse(@NonNull PlaceholderContext context, @NonNull ServerPlayer player, @NonNull Component message, @Nullable String arg) {
-        return escapeMMTags(message);
-    }
-
-    /**
-     * Escapes all minimessage tags in a component.
-     * @param component The component to escape tags in
-     * @return A version of the given component with escaped tags.
-     */
-    private Component escapeMMTags(Component component) {
-        Component newComp = component instanceof TextComponent text ?
-                Component.text(MiniMessage.miniMessage().escapeTags(text.content()))
-                        .style(component.style()) : component;
-
-        return newComp.children(component.children().stream()
-                .map(this::escapeMMTags)
-                .toList());
+        return message;
     }
 }
